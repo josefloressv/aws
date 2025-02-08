5107d5ab65f28b7a8eb414853921f57f8bf0ca29
José Flores <josefloressv@gmail.com>
2025-02-08 09:51:19 -0600

    Add Node.js application with Docker support and ECR deployment instructions

---
+++ apps/SampleApp_nodejs/Scripts/setup-nodejs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
+# Validate
+node -e "console.log('Running Node.js ' + process.version)"
